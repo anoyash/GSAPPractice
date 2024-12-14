c6f94374f8e75a97d6d9f3028e2e471b668eabd2
Ashutosh Kumar <ak2417942@gmail.com>
2024-12-14 19:15:48 +0530

    removed overlay layer , duration removed from animation and added pointerEvent to avoid multiple events

--- 4_cursor_animation/script.js
+++ 4_cursor_animation/script.js
@@ -3,10 +3,10 @@ var cursor = document.querySelector("#cursor");
 var myImg = document.querySelector("#myImage");
 
 mainDiv.addEventListener("mousemove", (params) => {
+  console.log("mouse event is called");
   gsap.to(cursor, {
     x: params.x,
     y: params.y,
-    duration: 0.5,
   });
 });
 
@@ -14,17 +14,23 @@ myImg.addEventListener("mouseenter", () => {
   console.log("mouse entered inside image");
   cursor.innerHTML = "View More";
   gsap.to(cursor, {
-    scale: 4,
+    height: "120px",
+    width: "120px",
     border: "1px solid white",
     backgroundColor: "transparent",
     color: "white",
-    duration: 0.1,
+    /* instead of adding overlay image, we can have do pointer Events:none as well 
+    the flicker effect happens due to multiple event listers working on the same time
+    */
+    pointerEvents: "none",
   });
 });
 
 myImg.addEventListener("mouseleave", () => {
   cursor.innerHTML = "";
   gsap.to(cursor, {
+    height: "40px",
+    width: "40px",
     scale: 1,
     opacity: 1,
     backgroundColor: "white",
