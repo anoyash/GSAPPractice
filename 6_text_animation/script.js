8a6b020a2d742ffdc942e3f5e47c0f71ef045dea
Ashutosh Kumar <ak2417942@gmail.com>
2024-12-14 23:31:19 +0530

    sheriyans -/+ stagger effect added in text animation

--- 6_text_animation/script.js
+++ 6_text_animation/script.js
@@ -1,11 +1,38 @@
 var myHeading = document.querySelector("h1");
 var splittedText = myHeading.textContent.split("");
 var spanedText = "";
+var leftText = "";
+var rightText = "";
 
-splittedText.forEach((char) => {
-  spanedText += "<span>" + char + "</span>";
+splittedText.forEach((char, idx) => {
+  // spanedText += "<span>" + char + "</span>";
+  if (idx < myHeading.textContent.length / 2)
+    leftText += `<span class="left-half">${char}</span>`;
+  else rightText += `<span class="right-half">${char}</span>`;
 });
 
-console.log(spanText);
+console.log(leftText);
+console.log(rightText);
 
-myHeading.innerHTML = spanedText;
+myHeading.innerHTML = leftText + rightText;
+
+// gsap.from("h1 span", {
+//   y: 50,
+//   opacity: 0,
+//   duration: 0.8,
+//   stagger: 0.3,
+// });
+
+gsap.from(".left-half", {
+  y: 50,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.3,
+});
+
+gsap.from(".right-half", {
+  y: 50,
+  opacity: 0,
+  duration: 0.5,
+  stagger: -0.3 /** for one element at a time in reverse order */,
+});
